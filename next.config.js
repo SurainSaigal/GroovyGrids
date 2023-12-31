8e8bec8e07b9f437d591fb657ecdc31360ecbba8
Surain Saigal <surainsaigal@gmail.com>
2023-12-31 15:52:05 -0600

    flask integration

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
             {
                 source: "/api/:path*",
                 destination:
-                    process.env.NODE_ENV === "development" || process.env.NODE_ENV === "production"
+                    process.env.NODE_ENV === "development"
                         ? "http://127.0.0.1:5328/api/:path*"
                         : "/api/",
             },
