236ca19bcb4cb008a3913ea88e3b59dadf24af1a
Surain Saigal <surainsaigal@gmail.com>
2024-07-16 16:17:19 -0500

    for kevin i oblige

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <body className={inter.className}>{children}</body>
+            <Analytics />
         </html>
     );
 }
