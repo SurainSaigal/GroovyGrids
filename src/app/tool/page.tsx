03903a7a1f2d19bf910e6e4f7559d0985c1fa86f
Surain Saigal <surainsaigal@gmail.com>
2024-07-17 14:52:57 -0500

    new domain config

--- src/app/tool/page.tsx
+++ src/app/tool/page.tsx
@@ -101,26 +101,6 @@ const TOOL = () => {
                     setImg(url);
                     // fetch(`/api/delete_image?filename=${data.filename}`);
                 }
-
-                // fetch(`data:image/jpeg;base64,${data.image}`)
-                //     .then((response) => response.blob())
-                //     .then((blob) => {
-                //         const url = URL.createObjectURL(blob);
-                //         setCachedImages((prevCachedImages) => ({
-                //             ...prevCachedImages,
-                //             [fetchCacheKey]: url,
-                //         }));
-                //         setCachedImageMaps((prevCachedImageMaps) => ({
-                //             ...prevCachedImageMaps,
-                //             [fetchCacheKey]: [imageResponses, data.dimensions],
-                //         }));
-                //         if (display) {
-                //             setDims(data.dimensions);
-                //             setImageMapDataDummy(imageResponses);
-                //             setCollageFailed(null);
-                //             setImg(url);
-                //         }
-                //     });
             })
             .catch((error) => {
                 if (error.name === "AbortError") {
